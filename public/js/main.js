ad509f2fc2e062d75f15ebec8435c3a5202c5190
jimmyzimltm <jimmy.zim@gmail.com>
2024-11-12 06:48:59 +0000

    InnerHTML done. Save before functioning session increment

--- public/js/main.js
+++ public/js/main.js
@@ -3,12 +3,14 @@ function resetVariables(){
     sessionStorage.setItem("GameStart", true)
 
     //healthstatus variables
-    sessionStorage.setItem("DayX",0)
+    sessionStorage.setItem("DayX",1)
     sessionStorage.setItem("BusinessOwnerConfidence",50)
     sessionStorage.setItem("ProblemStatementClarity",0)
     sessionStorage.setItem("CustomerInsight",0)
-    sessionStorage.setItem("TeamMorale",100)
+    sessionStorage.setItem("TeamMorale",70)
     sessionStorage.setItem("ProductReadiness","None")
+
+    sessionStorage.setItem("InterviewedPolicyOwner",0)
 
     sessionStorage.setItem("CustomerInterviews",0)
     sessionStorage.setItem("TeamSize",1)
@@ -24,9 +26,20 @@ function resetVariables(){
 //TODO - put in a function to clean up / wipe all articles as well, so that the Start button acts as reset as well
     //debug text
     alert("Initiating Day 1");
-    initiateEvent01()
+
+// Show the process menu
+    var hiddenDiv=document.getElementById("processmenu")
+    hiddenDiv.style.display='block';
+
+    document.getElementById("startButton").textContent = "Go back to Day 1";
+
+// Kick off Day 1 
+    initiateEvent01();
 }
 
+
+
+// repeated function to show the latest health status
 function updateHealth(){
     //debugger alert
     alert("Updating Health");
@@ -39,17 +52,44 @@ function updateHealth(){
     document.getElementById("business-owner-confidence").textContent=sessionStorage.getItem("BusinessOwnerConfidence")
 }
 
+
+// repeated function to move the events from mostrecent to below the line, and advance the day.
+function advancetheDay(){
+    // identify the current day
+    var dayX=+sessionStorage.getItem("DayX");
+    // move the most recent page to the matching element 
+    var eventID = "event"+sessionStorage.getItem("DayX")
+    var pastarticle = document.getElementById(eventID);
+    var article = document.getElementById("mostrecent");
+    var pastheader = pastarticle.querySelector("header h4");
+    var header = article.querySelector("header h4");
+    pastheader.textContent = header.textContent;
+    var pastparagraph = pastarticle.querySelector("p")
+    var paragraph = article.querySelector("p")
+    pastparagraph.innerHTML = paragraph.innerHTML
+    // and make the matching element visible
+    pastarticle.style.display = "block"
+    // advance the day count
+    //    dayX=dayX+1
+    //    sessionStorage.setItem("DayX", dayX)
+    sessionStorage.setItem("DayX",+sessionStorage.getItem("DayX")+1)
+    alert("After = Day " + sessionStorage.getItem("DayX"));
+}
+
+
 // this is a one-off function used to kick off with the first event - boss starting the project
 function initiateEvent01(){
 //    select Article with ID 1
-    var article = document.getElementById("1");
+    var article = document.getElementById("mostrecent");
     // change the header to reflect current Day
     var header = article.querySelector("header h4");
     header.textContent = "Day " + sessionStorage.getItem("DayX");
     // populate the event scenario
     var paragraph = article.querySelector("p")
-    paragraph.textContent ="Your RO, Patrick, assigns you to Projecct Manage a new web-app to distribute a new service to Singaporeans. He assigns you Andrea as your designer. He describes the new service briefly"
+    paragraph.textContent ="Your RO, Patrick, assigns you as Project Manager for a new web-app to distribute a new service to Singaporeans. He assigns you Andrea as your designer. He describes the new service briefly (Problem Statement Clarity +10)"
     // TODO - populate with more details
+    // make the article visible
+    article.style.display = "block"
     // change variables to reflect
     sessionStorage.setItem("TeamSize",2)
     var PSC =     +sessionStorage.getItem("ProblemStatementClarity")
@@ -58,8 +98,61 @@ function initiateEvent01(){
     updateHealth()
 }
 
-// this function is repeatedly called to show the menu of choices available in the highest 
-function populateChoices(){
+function interviewPolicyOwner(){
+    // move the mostrecent day into the past and increment the dayX
+    advancetheDay();
+
+    // get the current day
+    var dayX=+sessionStorage.getItem("DayX")
+    //    select Article with ID 1
+    var article = document.getElementById("mostrecent");
+    // change the header to reflect current Day
+    var header = article.querySelector("header h4");
+    var paragraph = article.querySelector("p")
+    header.textContent = "Day " + dayX;
+    
+    // resolve result of policy owner interview. Check conditionals
+    // Pathing - has the team interviewed policy owner already? If not, then check day -> early = good response, late = bad response. If yes, has the team learnt anything that necessitates coming back? If yes, good response, else bad response
+
+    // First conditional - has the team interviewed policy owner already? 
+    var InterviewedPolicyOwner =     +sessionStorage.getItem("InterviewedPolicyOwner")
+    var CustomerInsight = +sessionStorage.getItem("CustomerInsight")
+    if (InterviewedPolicyOwner ==0){
+        // check early or late
+        if (dayX<5){
+            // Good response - interview yields insights about problem statement and customer insight, builds business owner confidence and team morale
+            paragraph.innerHTML ="<p>You interview the Business Owner, Jeannette. The interview goes well. Jeannette shares deeper insights into the problems the policy team were seeing on the ground as well as their thoughts about customer behavior on the ground. Jeannette suggests you speak to the ground team to hear more about their experiences with customers as well.</p>"
+            paragraph.innerHTML +="<p class='health-status-growth'>Problem Statement Clarity + 20, Customer Insight +10</p>"
+            sessionStorage.setItem("ProblemStatementClarity",+sessionStorage.getItem("ProblemStatementClarity")+20)
+            sessionStorage.setItem("CustomerInsight",+sessionStorage.getItem("CustomerInsight")+10)
+            paragraph.innerHTML +="<p> At the end of the meeting, Jeannette shares that she is happy that the team had come to speak with her. After the meeting, Andrea tells you she has gained confidence in the project.</p>"
+            paragraph.innerHTML +="<p class='health-status-growth'>Business Owner Confidence + 10, Team Morale +10</p>"
+            sessionStorage.setItem("BusinessOwnerConfidence",+sessionStorage.getItem("BusinessOwnerConfidence")+10)
+            sessionStorage.setItem("TeamMorale",+sessionStorage.getItem("TeamMorale")+10)
+        } else {
+            // Bad response - business owner scolds you for coming to her so late. Still gets some insight about problem statement and customer insight, but loses business owner confidence and team morale
+            paragraph.innerHTML ="<p>You interview the Business Owner, Jeannette. The interview starts poorly. Jeannette shares her disappointment that you had delayed speaking to her and wonders aloud what you could possibly have been doing. Andrea is visibly shaken by her comments</p>"
+            paragraph.innerHTML +="<p class='health-status-loss'>Business Owner Confidence - 10, Team Morale - 10</p>"
+            paragraph.innerHTML +="<p> After she overcomes her anger, Jeannette shares some insights into the problems the policy team were seeing on the ground. While she still gives you more clarity about the problem statement, she seems very impatient and ends the meeting early. You cannot help but feel that some important information was left unsaid. </p>"
+            paragraph.innerHTML +="<p class='health-status-growth'>Problem Statement Clarity + 10</p>"           
+        }
+
+    } else {
+        // check level of customer insight
+        if (CustomerInsight>50){
+            // Good response - policy owner responds well to new insights, and clarifies their problem statement. Builds BOC and Morale
+
+        } else{
+            // Bad response - why are you wasting my time!
+        }
+    }
+    // Regardless, increment InterviewedPolicyOwner, store it, disable Interview if 2nd interview attempted
+
+        // update variables 
+
+    // Cleaunup option availability
 
 
+    // update the health status
+    updateHealth()
 }
