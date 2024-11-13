8b13340dc360681c2947648930ada4ec76203e96
jimmyzimltm <jimmy.zim@gmail.com>
2024-11-13 06:57:10 +0000

    2 buttons completed

--- public/js/main.js
+++ public/js/main.js
@@ -14,22 +14,35 @@ function resetVariables(){
     sessionStorage.setItem("InterviewedGroundDivision",0)
 
     sessionStorage.setItem("CustomerInterviews",0)
-    sessionStorage.setItem("TeamSize",1)
     sessionStorage.setItem("EngineersHired",false)
-    sessionStorage.setItem("ManDaysSpent",0)
 
     sessionStorage.setItem("PaperProto",false)
     sessionStorage.setItem("FigmaProto",false)
     sessionStorage.setItem("ClickableProto",false)
     //call once to show health variables
     updateHealth()
-//TODO - put in a function to clean up / wipe all articles as well, so that the Start button acts as reset as well
+//clean up / wipe all articles as well, so that the Start button acts as reset as well
+for (let eventday=1; eventday<31; eventday++) {
+    var article=document.getElementById("event"+eventday)
+    article.style.display="none"
+    var header = article.querySelector("header h4");
+    var paragraph = article.querySelector("p")
+    header.textContent=""
+    paragraph.textContent=""
+}
+
     //debug text
     alert("Initiating Day 1");
 
 // Show the process menu
     var hiddenDiv=document.getElementById("processmenu")
     hiddenDiv.style.display='block';
+    
+    var buttontoenable = document.getElementById("policyownerButton");    
+    buttontoenable.style.display = "block"
+    buttontoenable = document.getElementById("grounddivisionButton");    
+    buttontoenable.style.display = "block"
+
 
     document.getElementById("startButton").textContent = "Go back to Day 1";
 
@@ -107,18 +120,18 @@ function initiateEvent01(){
     header.textContent = "Day " + sessionStorage.getItem("DayX");
     // populate the event scenario
     var paragraph = article.querySelector("p")
-    paragraph.textContent ="Your RO, Patrick, assigns you as Project Manager for a new web-app to distribute a new service to Singaporeans. He assigns you Andrea as your designer. He describes the new service briefly (Problem Statement Clarity +10)"
+    paragraph.textContent ="Your RO, Patrick, assigns you as Project Manager for a new web-app to distribute a new service to Singaporeans. He assigns you Andrea as your designer, and tells you that when you have sufficient clarity on the solution, he will assign you engineers as well. He also describes the new service briefly and forwards you the slides showing the policy approval and intended solution."
+    paragraph.innerHTML +="<p class='health-status-growth'>Problem Statement Clarity + 10</p>"
+    changeSSV("ProblemStatementClarity", 10)
     // TODO - populate with more details
     // make the article visible
     article.style.display = "block"
-    // change variables to reflect
-    sessionStorage.setItem("TeamSize",2)
-    var PSC =     +sessionStorage.getItem("ProblemStatementClarity")
-    PSC = PSC + 10
-    sessionStorage.setItem("ProblemStatementClarity",PSC)
+    // update Health Status
     updateHealth()
 }
 
+
+// resolve result of policy owner interview. 
 function interviewPolicyOwner(){
     // move the mostrecent day into the past and increment the dayX
     advancetheDay();
@@ -130,8 +143,6 @@ function interviewPolicyOwner(){
     var header = article.querySelector("header h4");
     var paragraph = article.querySelector("p")
     header.textContent = "Day " + dayX;
-    
-    // resolve result of policy owner interview. Check conditionals
     // Pathing - has the team interviewed policy owner already? If not, then check day -> early = good response, late = bad response. If yes, has the team learnt anything that necessitates coming back? If yes, good response, else bad response
     // First conditional - has the team interviewed policy owner already? 
     var InterviewedPolicyOwner =     +sessionStorage.getItem("InterviewedPolicyOwner")
@@ -140,7 +151,7 @@ function interviewPolicyOwner(){
         // check early or late
         if (dayX<5){
             // Good response - interview yields insights about problem statement and customer insight, builds business owner confidence and team morale
-            paragraph.innerHTML ="<p>You interview the Business Owner, Jeannette. The interview goes well. Jeannette shares deeper insights into the problems the policy team were seeing on the ground as well as their thoughts about customer behavior on the ground. Jeannette suggests you speak to the ground team to hear more about their experiences with customers as well.</p>"
+            paragraph.innerHTML ="<p>You interview the Policy Director, Jeannette. The interview goes well. Jeannette shares deeper insights into the problems the policy team were seeing on the ground as well as their thoughts about customer behavior on the ground. Jeannette suggests you speak to the ground team to hear more about their experiences with customers as well.</p>"
             paragraph.innerHTML +="<p class='health-status-growth'>Problem Statement Clarity + 20, Customer Insight +10</p>"
             changeSSV("ProblemStatementClarity", 20)
             changeSSV("CustomerInsight",10)
@@ -153,7 +164,7 @@ function interviewPolicyOwner(){
             // sessionStorage.setItem("TeamMorale",+sessionStorage.getItem("TeamMorale")+10)
         } else {
             // Bad response - business owner scolds you for coming to her so late. Still gets some insight about problem statement and customer insight, but loses business owner confidence and team morale
-            paragraph.innerHTML ="<p>You interview the Business Owner, Jeannette. The interview starts poorly. Jeannette shares her disappointment that you had delayed speaking to her and wonders aloud what you could possibly have been doing. Andrea is visibly shaken by her comments</p>"
+            paragraph.innerHTML ="<p>You interview the Policy Director, Jeannette. The interview starts poorly. Jeannette shares her disappointment that you had delayed speaking to her and wonders aloud what you could possibly have been doing. Andrea is visibly shaken by her comments</p>"
             paragraph.innerHTML +="<p class='health-status-loss'>Business Owner Confidence - 10, Team Morale - 10</p>"
             changeSSV("BusinessOwnerConfidence", -10)
             changeSSV("TeamMorale",-10)
@@ -167,7 +178,7 @@ function interviewPolicyOwner(){
         // check level of customer insight
         if ((CustomerInsight-InterviewedPolicyOwner)>20){
             // Good response - policy owner responds well to new insights, and clarifies their problem statement. Builds BOC and Morale
-            paragraph.textContent =" 'Those are really interesting learning points. Jeannette is surprised by the insights you have unearthed and digs into the evidence you have assembled. She quickly regroups and lays out her thinking for how the web app should take these new learnings into account "
+            paragraph.textContent =" 'Those are really interesting learning points!' Jeannette is surprised by the insights you have unearthed and digs into the evidence you have assembled. She quickly regroups and lays out her thinking for how the web app should take these new learnings into account "
             paragraph.innerHTML +="<p class='health-status-growth'>Problem Statement Clarity + 10</p>"
             changeSSV("ProblemStatementClarity", 10)
         } else{
@@ -177,18 +188,16 @@ function interviewPolicyOwner(){
             changeSSV("BusinessOwnerConfidence", -10)
             changeSSV("TeamMorale",-10)
         }
-    }
-    // Regardless, increment InterviewedPolicyOwner, store it, 
-    changeSSV("InterviewedPolicyOwner", 1)
-    // disable Interview after 2nd interview 
-    if (sessionStorage.getItem("InterviewedPolicyOwner")=="2"){
+        // disable Interview after 2nd interview 
         var buttontodisable = document.getElementById("policyownerButton");    
         buttontodisable.style.display = "none"
+        
     }
     // update the health status
     updateHealth()
 }
 
+// resolve result of ground Division interview. 
 function interviewGroundDivision(){
     // move the mostrecent day into the past and increment the dayX
     advancetheDay();
@@ -201,58 +210,83 @@ function interviewGroundDivision(){
     var paragraph = article.querySelector("p")
     header.textContent = "Day " + dayX;
     
-    // resolve result of ground Division interview. Check conditionals
     // Pathing - has the team interviewed ground owner before? Has the team interviewed policy owner?  Has the team developed solutions or interviewed customer already?
     // First conditional - has the team interviewed policy owner already? 
     var InterviewedGroundDivision =     +sessionStorage.getItem("InterviewedGroundDivision")
     var CustomerInsight = +sessionStorage.getItem("CustomerInsight")
+    var InterviewedPolicyOwner =     +sessionStorage.getItem("InterviewedPolicyOwner")
+    var ProblemStatementClarity = +sessionStorage.getItem("ProblemStatementClarity")
     if (InterviewedGroundDivision ==0){
-        // check early or late
-        if (dayX<5){
-            // Good response - interview yields insights about problem statement and customer insight, builds business owner confidence and team morale
-            paragraph.innerHTML ="<p>You interview the Business Owner, Jeannette. The interview goes well. Jeannette shares deeper insights into the problems the policy team were seeing on the ground as well as their thoughts about customer behavior on the ground. Jeannette suggests you speak to the ground team to hear more about their experiences with customers as well.</p>"
-            paragraph.innerHTML +="<p class='health-status-growth'>Problem Statement Clarity + 20, Customer Insight +10</p>"
-            changeSSV("ProblemStatementClarity", 20)
-            changeSSV("CustomerInsight",10)
-            // sessionStorage.setItem("CustomerInsight",+sessionStorage.getItem("CustomerInsight")+10)
-            paragraph.innerHTML +="<p> At the end of the meeting, Jeannette shares that she is happy that the team had come to speak with her. After the meeting, Andrea tells you she has gained confidence in the project.</p>"
-            paragraph.innerHTML +="<p class='health-status-growth'>Business Owner Confidence + 10, Team Morale +10</p>"
-            changeSSV("BusinessOwnerConfidence", 10)
-            changeSSV("TeamMorale",10)
-            // sessionStorage.setItem("BusinessOwnerConfidence",+sessionStorage.getItem("BusinessOwnerConfidence")+10)
-            // sessionStorage.setItem("TeamMorale",+sessionStorage.getItem("TeamMorale")+10)
-        } else {
-            // Bad response - business owner scolds you for coming to her so late. Still gets some insight about problem statement and customer insight, but loses business owner confidence and team morale
-            paragraph.innerHTML ="<p>You interview the Business Owner, Jeannette. The interview starts poorly. Jeannette shares her disappointment that you had delayed speaking to her and wonders aloud what you could possibly have been doing. Andrea is visibly shaken by her comments</p>"
+        // have the team interviewed the policy owner?
+        if (InterviewedPolicyOwner ==0){
+            // Not good response - ground division does not quite understand the policy decision. However, they are able to share their views on what the customer journey looks like
+            paragraph.innerHTML ="<p>You interview YT, the Director of the customer-facing division. She has great insights into the customers who might be using the product. </p>"
+            paragraph.innerHTML +="<p class='health-status-growth'>Customer Insight +20</p>"
+            changeSSV("CustomerInsight",20)
+            paragraph.innerHTML +="<p> However, she questions the proposed solution, and says that she thinks it does not address the pain points of customers and the staff who inevitably face the brunt of customer unhappiness. Without a clear understanding of the policy rationale behind the decision, you find it difficult to have a deeper discussion with her on how the web app would work. You sense that she may be a bit frustrated. Andrea's confidence is also shaken.</p>"
             paragraph.innerHTML +="<p class='health-status-loss'>Business Owner Confidence - 10, Team Morale - 10</p>"
             changeSSV("BusinessOwnerConfidence", -10)
             changeSSV("TeamMorale",-10)
-            paragraph.innerHTML +="<p> After she overcomes her anger, Jeannette shares some insights into the problems the policy team were seeing on the ground. While she still gives you more clarity about the problem statement, she seems very impatient and ends the meeting early. You cannot help but feel that some important information was left unsaid. </p>"
-            paragraph.innerHTML +="<p class='health-status-growth'>Problem Statement Clarity + 10</p>"
+        } else {
+            // Good response. With knowledge of the policy goals that the policy division intended, you manage to dig deeper into the problem statement. 
+            paragraph.innerHTML ="<p>You interview YT, the Director of the customer-facing division. She has great insights into the customers who might be using the product.</p>"
+            paragraph.innerHTML +="<p class='health-status-growth'>Customer Insight +20</p>"
+            changeSSV("CustomerInsight",20)
+            paragraph.innerHTML +="<p> YT questions the proposed solution,and says that she thinks it does not address the pain points of customers and the staff who inevitably face the brunt of customer unhappiness. From your discussions with Jeannette, you recall that the policy division had highlighted that they felt that customer complaints on the ground were precipitated upstream by the behaviours of a different group of stakeholders. The proposed solution aims to address the behaviors of those stakeholders and eliminate the upstream cause of the issues which trigger customer complaints. </p>"
+            paragraph.innerHTML +="<p> When you have explained that, YT thoughtfully acknowledges the view. She explains that the upstream issue is more complicated than the policy division thinks. Nonetheless, you convince her that studying the customers' behaviours more closely may shed more light on the possible solutions.   </p>"
+            paragraph.innerHTML +="<p class='health-status-growth'>Problem Statement Clarity + 10, Business Owner Confidence + 10</p>"
             changeSSV("ProblemStatementClarity", 10)           
+            changeSSV("BusinessOwnerConfidence", 10)
         }
+        //either way, store the current customer insight level as the variable for when we last interviewed the ground division
+        sessionStorage.setItem("InterviewedGroundDivision",sessionStorage.getItem("CustomerInsight"))
+
     } else {
-        // check level of customer insight
-        if ((CustomerInsight-InterviewedPolicyOwner)>20){
-            // Good response - policy owner responds well to new insights, and clarifies their problem statement. Builds BOC and Morale
-            paragraph.textContent =" 'Those are really interesting learning points. Jeannette is surprised by the insights you have unearthed and digs into the evidence you have assembled. She quickly regroups and lays out her thinking for how the web app should take these new learnings into account "
-            paragraph.innerHTML +="<p class='health-status-growth'>Problem Statement Clarity + 10</p>"
-            changeSSV("ProblemStatementClarity", 10)
+        // check if level of customer insight has changed significantly since you last met her
+        if ((CustomerInsight-InterviewedGroundDivision)>20){
+            // Good response
+            paragraph.textContent =" 'Thank you for reporting back on your findings. It looks like, we should be pursuing a different track than any of us had originally planned.'  YT feels vindicated by your findings, that the original policy solution planned would not work."
+            paragraph.innerHTML +="<p class='health-status-growth'>Business Owner Confidence + 10</p>"
+            changeSSV("Business Owner Confidence", 10)
+            // with sufficient problem statement clarity, you can propose a solution and validate it with her
+            if (ProblemStatementClarity>60){
+                paragraph.innerHTML +="<p> With the insights you had accumulated on the Problem Statement Clarity, you have a working hypothesis on how you would like to position the web app. You bounce the idea off YT and she helps you to quickly fill in with her knowledge of the customers</p>"
+                paragraph.innerHTML +="<p class='health-status-growth'>Customer Insight + 10</p>"
+                changeSSV("CustomerInsight", 10)
+            } else{
+                //nothing for now
+            }
         } else{
             // Bad response - why are you wasting my time!
-            paragraph.textContent =" 'I thought I just saw you recently?' Jeannette is confused and surprised why you have come to see her again when you have no new insights to add, and chews you and the entire team out for wasting her time"
-            paragraph.innerHTML +="<p class='health-status-loss'>Business Owner Confidence - 10, Team Morale - 10</p>"
+            paragraph.textContent =" 'I do not understand why we are meeting again, when you have not done any further testing with customers.' YT is impatient and rather upset that you did not do sufficient customer groundwork before meeting her again."
+            paragraph.innerHTML +="<p class='health-status-loss'>Business Owner Confidence - 10</p>"
             changeSSV("BusinessOwnerConfidence", -10)
-            changeSSV("TeamMorale",-10)
         }
-    }
-    // Regardless, increment InterviewedPolicyOwner, store it, 
-    changeSSV("InterviewedPolicyOwner", 1)
-    // disable Interview after 2nd interview 
-    if (sessionStorage.getItem("InterviewedPolicyOwner")=="2"){
-        var buttontodisable = document.getElementById("policyownerButton");    
+        // disable Interview after 2nd interview 
+        var buttontodisable = document.getElementById("grounddivisionButton");    
         buttontodisable.style.display = "none"
+        
     }
+    // update the health status
+    updateHealth()
+}
+
+
+function observeCustomers(){
+    // move the mostrecent day into the past and increment the dayX
+    advancetheDay();
+    // get the current day
+    var dayX=+sessionStorage.getItem("DayX")
+    //    select Article with ID mostrecent
+    var article = document.getElementById("mostrecent");
+    // change the header to reflect current Day
+    var header = article.querySelector("header h4");
+    var paragraph = article.querySelector("p")
+    header.textContent = "Day " + dayX;
+    // Pathing - how much customerinsight and problemstatement clarity does the team have? 
+
+    // disable customerObservations
+    
     // update the health status
     updateHealth()
 }
